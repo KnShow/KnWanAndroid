5955d10e369ad838893ceb5f3772401260fc7d5a
01-00240-PC\01-00240 <554193697@qq.com>
2018-08-13 10:46:13 +0800

    ......

--- app/src/main/java/wanandroid/kn/cn/knwanandroid/temp/HomeFragment.java
+++ app/src/main/java/wanandroid/kn/cn/knwanandroid/temp/HomeFragment.java
@@ -14,6 +14,6 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, null);
+        return inflater.inflate(R.layout.fragment_home,  null);
     }
 }
